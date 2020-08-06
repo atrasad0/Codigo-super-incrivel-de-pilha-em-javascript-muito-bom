450b9f78820b69d69b1a9a7aee222728ea817070
atrasad0 <yurijnr@gmail.com>
2020-08-05 23:11:33 -0300

    js pilha

---
+++ pilhaYuriGoncalves.js
@@ -0,0 +1,57 @@
+//Yuri Goncalves
+alert("!!Saida no console!!")
+function Pilha() {
+    this.pilha = [];
+    this.insere = param => {
+        if((isInt(param))) {
+            let value = parseInt(param);
+            this.pilha[this.pilha.length] = param;
+        }
+        else{
+            console.log("Nao e permitido letras, insira um numero")
+            return;
+        }
+    }
+    this.remove = () => {
+        if (this.pilha.length > 0) {
+            //na ultima posicao remove o ultimo
+            this.pilha.splice(this.pilha.length - 1, 1);
+        }
+        else {
+            console.log("Pilha Vazia");
+            return;
+        }
+    }
+    this.menorElemento = () => {
+        if (this.pilha.length > 0) {
+            //ordena a pilha; ps: uma vez que o sort() ordena um array em ordem alfabética passo a condicao callback
+            this.pilha.sort((a,b)=>a-b);
+            //retorna menor valor
+            return this.pilha[0];
+        }
+    }
+}
+
+//Verifica se é um numero
+function isInt(value) {
+    return !isNaN(parseInt(value));
+}
+
+let p = new Pilha();
+
+p.insere("abc");
+
+p.insere(8);
+p.insere(2);
+p.insere(9);
+p.insere(30);
+p.insere(0);
+
+console.log(p);
+
+//remove o 0 "LIFO"
+p.remove();
+
+let menor = p.menorElemento();
+
+console.log("Menor Elemento: ", menor);
